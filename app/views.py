3fb803e2c927d6babef31f422f0cedaa204daead
Iulian <julian.stana@gmail.com>
2017-01-11 00:53:20 +0200

    [pdf_saver] using the rending string save string to pdf

--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 from flask import render_template
+from pdfkit import from_string
 
 from app import app
+from config import pdf_options
 
 
 @app.route('/', methods=['GET'])
 def __index__():
-    return render_template('water_notice.html', nr_apartament=79,
-                           tip_apa="rece", numar=1, start_time=1, end_time=2)
+    cold_notice = render_template('water_notice.html', nr_apartament=79,
+                                  tip_apa="rece", numar=1, start_time=1,
+                                  end_time=2)
+    from_string(cold_notice, "test.pdf", options=pdf_options)
+    return cold_notice
 
--- config.py
+++ config.py
@@ -5,3 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you will never guess'
+
+# pdfkit options 
+pdf_options = {
+    'page-size': 'A4',
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in',
+}
+
+

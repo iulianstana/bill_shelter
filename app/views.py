1a9b3db432545517daf44335ff2a96e90a8cc0b7
Iulian <julian.stana@gmail.com>
2017-01-11 00:47:45 +0200

    [water_notice] create a template that holds my table and render it in root path

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/', methods=['GET'])
 def __index__():
-    return "Hello World"
+    return render_template('water_notice.html', nr_apartament=79,
+                           tip_apa="rece", numar=1, start_time=1, end_time=2)
 

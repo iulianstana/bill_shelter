61092992e2bf225da8efa1182b5c92ec7cbde2bc
Iulian <julian.stana@gmail.com>
2017-01-12 00:46:23 +0200

    [template] send a dictionary with result values to template and render it

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import render_template
 from pdfkit import from_string
 
@@ -5,11 +7,29 @@ from app import app
 from config import pdf_options
 
 
+def get_notice_data(bath_old, bath_new, kitchen_old, kitchen_new):
+    time_frame = ['Ianuarie', 'Februarie']
+    notice_number = '10'
+    current_date = datetime.now()
+    result = {
+        'nr_apartment': 79,
+        'notice_number': notice_number.center(6, " "),
+        'start_time': time_frame[0].center(30, " "),
+        'end_time': time_frame[1].center(30, " "),
+        'bath_old': bath_old,
+        'bath_new': bath_new,
+        'kitchen_old': kitchen_old,
+        'kitchen_new': kitchen_new,
+        'type': 'Rece'.center(15, " "),
+        "completed_date": current_date.strftime("%Y-%m-%d").center(20, " "),
+    }
+    cold_notice = render_template('water_notice.html', result=result)
+    return cold_notice
+
+
 @app.route('/', methods=['GET'])
 def __index__():
-    cold_notice = render_template('water_notice.html', nr_apartament=79,
-                                  tip_apa="rece", numar=1, start_time=1,
-                                  end_time=2)
-    from_string(cold_notice, "test.pdf", options=pdf_options)
+    cold_notice = get_notice_data(1139.472, 1145.128, 226.892, 227.515)
+    from_string(cold_notice, "water_shelter/test.pdf", options=pdf_options)
     return cold_notice
 

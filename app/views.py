34a44466e58e408ac25ffd1463e58375c57415be
Iulian <julian.stana@gmail.com>
2017-01-12 23:42:02 +0200

    [template] reduce redundancy

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ from app import app
 from config import pdf_options
 
 
-def get_notice_data(bath_old, bath_new, kitchen_old, kitchen_new):
+def get_notice_data(bath_old, bath_new, kitchen_old, kitchen_new, water_type):
     time_frame = ['Ianuarie', 'Februarie']
     notice_number = '10'
     current_date = datetime.now()
@@ -20,16 +20,17 @@ def get_notice_data(bath_old, bath_new, kitchen_old, kitchen_new):
         'bath_new': bath_new,
         'kitchen_old': kitchen_old,
         'kitchen_new': kitchen_new,
-        'type': 'Rece'.center(15, " "),
+        'type': water_type.center(15, " "),
         "completed_date": current_date.strftime("%Y-%m-%d").center(20, " "),
     }
-    cold_notice = render_template('water_notice.html', result=result)
-    return cold_notice
+    return result
 
 
 @app.route('/', methods=['GET'])
 def __index__():
-    cold_notice = get_notice_data(1139.472, 1145.128, 226.892, 227.515)
-    from_string(cold_notice, "water_shelter/test.pdf", options=pdf_options)
-    return cold_notice
+    cold_notice = get_notice_data(1139.472, 1145.128, 226.892, 227.515, 'APA RECE')
+    hot_notice = get_notice_data(632.34, 635.287, 351.359, 352.355, 'APA CALDA')
+    render_obj = render_template('water_notice.html', notices=[cold_notice, hot_notice])
+    #from_string(render_obj, "water_shelter/test.pdf", options=pdf_options)
+    return render_obj
 

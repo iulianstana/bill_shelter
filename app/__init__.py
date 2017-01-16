ffc3fafa97c62a2b3ab43cbc562a23ac2ed88a17
Iulian <julian.stana@gmail.com>
2017-01-16 21:39:03 +0200

    [template] convert timestamp to Month-Year format

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from .momentjs import momentjs
+
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+app.jinja_env.globals['momentjs'] = momentjs
+
 
 from app import views, models
